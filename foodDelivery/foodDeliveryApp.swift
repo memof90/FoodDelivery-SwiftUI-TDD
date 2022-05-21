4172e2d6c76f92711b770cf9b7e024c27449b62d
memo figueredo <memof80@gmail.com>
2022-05-20 19:22:41 -0500

    Initial setup

---
+++ foodDelivery/foodDeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  foodDeliveryApp.swift
+//  foodDelivery
+//
+//  Created by Memo Figueredo on 20/5/22.
+//
+
+import SwiftUI
+
+@main
+struct foodDeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
